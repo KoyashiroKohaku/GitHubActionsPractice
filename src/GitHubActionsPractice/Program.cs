9b8fd1f41bf3622342a65b20a43103910fad326b
Koyashiro Kohaku <develop@koyashiro.net>
2020-07-12 00:12:20 +0900

    feat: create .NET Core project

---
+++ src/GitHubActionsPractice/Program.cs
@@ -0,0 +1,26 @@
+using System;
+
+namespace KoyashiroKohaku.GitHubActionsPractice
+{
+    public static class Program
+    {
+        static void Main(string[] args)
+        {
+            // Hello World!
+            Console.WriteLine(GetHellowWorld());
+
+            // The current time is 'yyyy/MM/dd H:mm:ss'
+            Console.WriteLine($"The current time is '{GetCurrentTime()}'.");
+        }
+
+        public static string GetHellowWorld()
+        {
+            return "Hello World!";
+        }
+
+        public static DateTime GetCurrentTime()
+        {
+            return DateTime.Now;
+        }
+    }
+}
---
+++ test/GitHubActionsPractice.Test/UnitTest1.cs
@@ -0,0 +1,23 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using KoyashiroKohaku.GitHubActionsPractice;
+using System;
+
+namespace KoyashiroKohaku.GitHubActionsPractice.Test
+{
+    [TestClass]
+    public class ProgramTest
+    {
+        [TestMethod]
+        public void GetHellowWorldTest()
+        {
+            Assert.AreEqual("Hello World!", Program.GetHellowWorld());
+        }
+
+        [TestMethod]
+        public void GetCurrentTimeTest()
+        {
+            var timeSpan = Program.GetCurrentTime() - DateTime.Now;
+            Assert.IsTrue(-10 < timeSpan.Seconds && timeSpan.Seconds < 10);
+        }
+    }
+}
